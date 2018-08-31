419ab50c90ee658bbd5fa46437ef5d9ca2ad3e93
Flatiron School <andrewsprague@outlook.com>
2018-08-31 09:00:39 -0500

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,20 @@
+class Appointment
+
+    attr_reader :name
+    attr_accessor :doctor, :date, :patient
+
+    @@all = []
+
+    def initialize (patient, doctor, date)
+        @patient = patient
+        @doctor = doctor
+        @date = date
+        
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+end
---
+++ lib/artist.rb
@@ -0,0 +1,39 @@
+require 'pry'
+
+class Artist
+
+    attr_reader :name
+
+    @@all = []
+
+    def initialize (name)
+        @name = name
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end 
+
+    def new_song(name, genre)
+        song = Song.new(name, self, genre)
+    end
+
+    def songs
+        Song.all.select do |song|
+            song.artist == self
+        end
+    end
+
+    def genres
+        # binding.pry
+        self.songs.map do |song|
+            song.genre
+        end 
+    end
+end
+
+drake = Artist.new('drake')
+
+# binding.pry
